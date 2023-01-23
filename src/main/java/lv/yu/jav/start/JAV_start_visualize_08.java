11813773cf6eb83ce83e7697f6e9ca703c536aac
Yuri UTKIN <yu-2023@inbox.lv>
2023-01-23 15:28:34 +0200

    Add files via upload

---
+++ src/main/java/lv/yu/jav/start/JAV_start_visualize_08.java
@@ -0,0 +1,96 @@
+
+/**
+ * 
+ * Java package     lv.yu.jav.start
+ * 
+ * Java program     JAV_start_visualize_08.java     Apache License 2.0
+ *      
+ * Copyright (c)    Yuri Utkin 2023                 mob.+371 12345678     https://www.jago.lv
+ * 
+ */
+package lv.yu.jav.start;
+
+import java.awt.BorderLayout;
+
+/**
+ * JAV_start_visualize_08
+ */
+public class JAV_start_visualize_08 {
+	
+/**
+ * JAV_start_visualize_08()
+ */
+    public JAV_start_visualize_08() {
+
+//--------------------------------------------------    	
+    	
+    new lv.yu.jav.start.JAV_start_08();
+		     		
+//--------------------------------------------------        
+    
+        lv.yu.jav.JAV_08_internal_frames.desktoppane_11.removeAll();
+    
+    new lv.yu.jav.start.start_11.JAV_start_11_08();
+    		                
+        lv.yu.jav.JAV_08_internal_frames.desktoppane_11.add(lv.yu.jav.start.start_11.JAV_start_11_08.JInternalFrame_start_11, BorderLayout.CENTER);
+                   
+        lv.yu.jav.JAV_08_internal_frames.panel_1.add(lv.yu.jav.JAV_08_internal_frames.desktoppane_11, BorderLayout.WEST);
+
+//--------------------------------------------------          
+    
+        lv.yu.jav.JAV_08_internal_frames.desktoppane_12.removeAll();
+        
+    new lv.yu.jav.start.start_12.JAV_start_12_08();
+    	                	
+        lv.yu.jav.JAV_08_internal_frames.desktoppane_12.add(lv.yu.jav.start.start_12.JAV_start_12_08.JInternalFrame_start_12, BorderLayout.CENTER);
+
+        lv.yu.jav.JAV_08_internal_frames.panel_1.add(lv.yu.jav.JAV_08_internal_frames.desktoppane_12, BorderLayout.CENTER);
+
+//--------------------------------------------------        
+        
+        lv.yu.jav.JAV_08_internal_frames.desktoppane_13.removeAll();
+        
+    new lv.yu.jav.start.start_13.JAV_start_13_08();
+       		                    
+        lv.yu.jav.JAV_08_internal_frames.desktoppane_13.add(lv.yu.jav.start.start_13.JAV_start_13_08.JInternalFrame_start_13, BorderLayout.CENTER);
+        
+        lv.yu.jav.JAV_08_internal_frames.panel_1.add(lv.yu.jav.JAV_08_internal_frames.desktoppane_13, BorderLayout.EAST);
+
+//--------------------------------------------------
+        
+        lv.yu.jav.JAV_08_internal_frames.desktoppane_21.removeAll();
+    
+    new lv.yu.jav.start.start_21.JAV_start_21_08();
+    		                
+        lv.yu.jav.JAV_08_internal_frames.desktoppane_21.add(lv.yu.jav.start.start_21.JAV_start_21_08.JInternalFrame_start_21, BorderLayout.CENTER);
+         
+        lv.yu.jav.JAV_08_internal_frames.panel_2.add(lv.yu.jav.JAV_08_internal_frames.desktoppane_21, BorderLayout.WEST);
+
+//--------------------------------------------------         
+
+        lv.yu.jav.JAV_08_internal_frames.desktoppane_22.removeAll();
+    
+    new lv.yu.jav.start.start_22.JAV_start_22_08();
+    		                
+        lv.yu.jav.JAV_08_internal_frames.desktoppane_22.add(lv.yu.jav.start.start_22.JAV_start_22_08.JInternalFrame_start_22, BorderLayout.CENTER);
+          
+        lv.yu.jav.JAV_08_internal_frames.panel_2.add(lv.yu.jav.JAV_08_internal_frames.desktoppane_22, BorderLayout.CENTER);
+
+//--------------------------------------------------   
+
+        lv.yu.jav.JAV_08_internal_frames.desktoppane_23.removeAll();
+    
+    new lv.yu.jav.start.start_23.JAV_start_23_08();
+    		                
+        lv.yu.jav.JAV_08_internal_frames.desktoppane_23.add(lv.yu.jav.start.start_23.JAV_start_23_08.JInternalFrame_start_23, BorderLayout.CENTER);
+        
+        lv.yu.jav.JAV_08_internal_frames.panel_2.add(lv.yu.jav.JAV_08_internal_frames.desktoppane_23, BorderLayout.EAST);
+
+//--------------------------------------------------          
+			            		
+		lv.yu.jav.JAV_08_internal_frames.frame.setVisible(true); 
+
+    }  //  end JAV_start_visualize_08()
+   
+}  //  end JAV_start_visualize_08
+
