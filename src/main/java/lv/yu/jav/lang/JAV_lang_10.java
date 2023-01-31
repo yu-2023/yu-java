aed3f36b5df5ec92fed4c2ebc0d2fc86d3f7ccd3
Yuri UTKIN <yu-2023@inbox.lv>
2023-01-31 12:47:00 +0200

    Update JAV_lang_10.java

--- src/main/java/lv/yu/jav/lang/JAV_lang_10.java
+++ src/main/java/lv/yu/jav/lang/JAV_lang_10.java
@@ -22,17 +22,17 @@ public class JAV_lang_10 {
     	
     new lv.yu.jav.start.JAV_start_menu_10();
     
-	new lv.yu.jav.start.JAV_start_menu_start_10();
+    new lv.yu.jav.start.JAV_start_menu_start_10();
     new lv.yu.jav.load.JAV_load_menu_start_10();
     new lv.yu.jav.xxxxx.JAV_xxxxx_menu_start_10();
     
-	new lv.yu.jav.lang.JAV_lang_menu_lang_10();
+    new lv.yu.jav.lang.JAV_lang_menu_lang_10();
 	
-	new lv.yu.jav.help.JAV_help_menu_help_10();
-	new lv.yu.jav.lang.JAV_lang_menu_lang_10();
+    new lv.yu.jav.help.JAV_help_menu_help_10();
+    new lv.yu.jav.lang.JAV_lang_menu_lang_10();
     new lv.yu.jav.exit.JAV_exit_menu_exit_10();
 	
-	lv.yu.jav.JAV_10_tabs.frame.setJMenuBar(lv.yu.jav.JAV_10_tabs.menubar);
+    lv.yu.jav.JAV_10_tabs.frame.setJMenuBar(lv.yu.jav.JAV_10_tabs.menubar);
 
       }  //  end JAV_lang_10()
         
