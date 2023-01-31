47ffb740d375624cc013bcf369ec5256b779ac7f
Yuri UTKIN <yu-2023@inbox.lv>
2023-01-31 14:43:57 +0200

    Update JAV_load_menu_load_2_09.java

--- src/main/java/lv/yu/jav/load/JAV_load_menu_load_2_09.java
+++ src/main/java/lv/yu/jav/load/JAV_load_menu_load_2_09.java
@@ -34,13 +34,13 @@ public class JAV_load_menu_load_2_09 {
         var menuitem_Lo21 = new JMenuItem("Lo21");
             menuitem_Lo21.setToolTipText("Lo21 application");
             menuitem_Lo21.addActionListener(new ActionListener() {
-	            public void actionPerformed(ActionEvent e) { System.out.println("Selected menu: " + e.getActionCommand()); } } );
+                public void actionPerformed(ActionEvent e) { System.out.println("Selected menu: " + e.getActionCommand()); } } );
             menu_Load_2.add(menuitem_Lo21);
 
         var menuitem_Lo22 = new JMenuItem("Lo22");
             menuitem_Lo22.setToolTipText("Lo22 application");
             menuitem_Lo22.addActionListener(new ActionListener() {
-            	public void actionPerformed(ActionEvent e) { System.out.println("Selected menu: " + e.getActionCommand()); } } );
+                public void actionPerformed(ActionEvent e) { System.out.println("Selected menu: " + e.getActionCommand()); } } );
             menu_Load_2.add(menuitem_Lo22);
 
             lv.yu.jav.JAV_09_internal_frames_change.menubar.add(menu_Load_2);
