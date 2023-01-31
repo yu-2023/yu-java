9f22d1cf36832c177c1c1a57585401670de74e27
Yuri UTKIN <yu-2023@inbox.lv>
2023-01-31 15:10:48 +0200

    Update JAV_start_22_10.java

--- src/main/java/lv/yu/jav/start/start_22/JAV_start_22_10.java
+++ src/main/java/lv/yu/jav/start/start_22/JAV_start_22_10.java
@@ -25,7 +25,7 @@ public final class JAV_start_22_10 {
 /**
  * JTabbedPane_start_22
  */
-	public static JTabbedPane JTabbedPane_start_22 = new JTabbedPane();
+    public static JTabbedPane JTabbedPane_start_22 = new JTabbedPane();
 
 /**
  * JInternalFrame_start_22
