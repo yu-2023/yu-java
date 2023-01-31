5a9c22e89037aebe540eb303863e08080e70a791
Yuri UTKIN <yu-2023@inbox.lv>
2023-01-31 15:19:41 +0200

    Update JAV_start_10.java

--- src/main/java/lv/yu/jav/start/JAV_start_10.java
+++ src/main/java/lv/yu/jav/start/JAV_start_10.java
@@ -20,21 +20,21 @@ public class JAV_start_10 {
  */
     public JAV_start_10() {
     	
-	new lv.yu.jav.start.JAV_start_menu_10();
+    new lv.yu.jav.start.JAV_start_menu_10();
 	
-	new lv.yu.jav.start.JAV_start_menu_start_10();
-	new lv.yu.jav.load.JAV_load_menu_start_10();
-	new lv.yu.jav.xxxxx.JAV_xxxxx_menu_start_10();
+    new lv.yu.jav.start.JAV_start_menu_start_10();
+    new lv.yu.jav.load.JAV_load_menu_start_10();
+    new lv.yu.jav.xxxxx.JAV_xxxxx_menu_start_10();
 
     new lv.yu.jav.start.JAV_start_menu_start_1_10();
     new lv.yu.jav.start.JAV_start_menu_start_2_10();
     new lv.yu.jav.start.JAV_start_menu_start_3_10();
 	
-	new lv.yu.jav.help.JAV_help_menu_help_10();
-	new lv.yu.jav.lang.JAV_lang_menu_lang_10();
+    new lv.yu.jav.help.JAV_help_menu_help_10();
+    new lv.yu.jav.lang.JAV_lang_menu_lang_10();
     new lv.yu.jav.exit.JAV_exit_menu_exit_10();
 	
-	lv.yu.jav.JAV_10_tabs.frame.setJMenuBar(lv.yu.jav.JAV_10_tabs.menubar);
+    lv.yu.jav.JAV_10_tabs.frame.setJMenuBar(lv.yu.jav.JAV_10_tabs.menubar);
 
       }  //  end JAV_start_10()
         
