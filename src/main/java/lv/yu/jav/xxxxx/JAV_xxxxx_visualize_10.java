be88516df409f4bef0420b44a7eab5f6e6cec069
Yuri UTKIN <yu-2023@inbox.lv>
2023-02-01 11:59:36 +0200

    Update JAV_xxxxx_visualize_10.java

--- src/main/java/lv/yu/jav/xxxxx/JAV_xxxxx_visualize_10.java
+++ src/main/java/lv/yu/jav/xxxxx/JAV_xxxxx_visualize_10.java
@@ -28,11 +28,11 @@ public class JAV_xxxxx_visualize_10 {
 		            	
 //--------------------------------------------------
     
-    	lv.yu.jav.JAV_10_tabs.desktoppane_11.removeAll();
+        lv.yu.jav.JAV_10_tabs.desktoppane_11.removeAll();
         	
     new lv.yu.jav.xxxxx.xxxxx_11.JAV_xxxxx_11_10();
             	    		            
-	    lv.yu.jav.JAV_10_tabs.desktoppane_11.add(lv.yu.jav.xxxxx.xxxxx_11.JAV_xxxxx_11_10.JInternalFrame_xxxxx_11, BorderLayout.CENTER);
+        lv.yu.jav.JAV_10_tabs.desktoppane_11.add(lv.yu.jav.xxxxx.xxxxx_11.JAV_xxxxx_11_10.JInternalFrame_xxxxx_11, BorderLayout.CENTER);
 
 //--------------------------------------------------
             
@@ -40,11 +40,11 @@ public class JAV_xxxxx_visualize_10 {
         
     new lv.yu.jav.xxxxx.xxxxx_12.JAV_xxxxx_12_10();
         
-	    lv.yu.jav.JAV_10_tabs.desktoppane_12.add(lv.yu.jav.xxxxx.xxxxx_12.JAV_xxxxx_12_10.JInternalFrame_xxxxx_12, BorderLayout.CENTER);
+        lv.yu.jav.JAV_10_tabs.desktoppane_12.add(lv.yu.jav.xxxxx.xxxxx_12.JAV_xxxxx_12_10.JInternalFrame_xxxxx_12, BorderLayout.CENTER);
     
 //--------------------------------------------------                
 
-	    lv.yu.jav.JAV_10_tabs.desktoppane_13.removeAll();
+        lv.yu.jav.JAV_10_tabs.desktoppane_13.removeAll();
         	
     new lv.yu.jav.xxxxx.xxxxx_13.JAV_xxxxx_13_10();
             	    		            
@@ -52,19 +52,19 @@ public class JAV_xxxxx_visualize_10 {
 
 //--------------------------------------------------
         
-	    lv.yu.jav.JAV_10_tabs.desktoppane_21.removeAll();
+        lv.yu.jav.JAV_10_tabs.desktoppane_21.removeAll();
         	
     new lv.yu.jav.xxxxx.xxxxx_21.JAV_xxxxx_21_10();
             	    		            
-	    lv.yu.jav.JAV_10_tabs.desktoppane_21.add(lv.yu.jav.xxxxx.xxxxx_21.JAV_xxxxx_21_10.JInternalFrame_xxxxx_21, BorderLayout.CENTER);
+        lv.yu.jav.JAV_10_tabs.desktoppane_21.add(lv.yu.jav.xxxxx.xxxxx_21.JAV_xxxxx_21_10.JInternalFrame_xxxxx_21, BorderLayout.CENTER);
 
 //--------------------------------------------------
         
-	    lv.yu.jav.JAV_10_tabs.desktoppane_22.removeAll();
+        lv.yu.jav.JAV_10_tabs.desktoppane_22.removeAll();
         
     new lv.yu.jav.xxxxx.xxxxx_22.JAV_xxxxx_22_10();
         
-	    lv.yu.jav.JAV_10_tabs.desktoppane_22.add(lv.yu.jav.xxxxx.xxxxx_22.JAV_xxxxx_22_10.JInternalFrame_xxxxx_22, BorderLayout.CENTER);
+        lv.yu.jav.JAV_10_tabs.desktoppane_22.add(lv.yu.jav.xxxxx.xxxxx_22.JAV_xxxxx_22_10.JInternalFrame_xxxxx_22, BorderLayout.CENTER);
     
 //--------------------------------------------------	                
 
@@ -72,11 +72,11 @@ public class JAV_xxxxx_visualize_10 {
         	
     new lv.yu.jav.xxxxx.xxxxx_23.JAV_xxxxx_23_10();
             	    		            
-	    lv.yu.jav.JAV_10_tabs.desktoppane_23.add(lv.yu.jav.xxxxx.xxxxx_23.JAV_xxxxx_23_10.JInternalFrame_xxxxx_23, BorderLayout.CENTER);
+        lv.yu.jav.JAV_10_tabs.desktoppane_23.add(lv.yu.jav.xxxxx.xxxxx_23.JAV_xxxxx_23_10.JInternalFrame_xxxxx_23, BorderLayout.CENTER);
 
 //--------------------------------------------------
 		            		            		            	
-	    lv.yu.jav.JAV_10_tabs.frame.setVisible(true);
+        lv.yu.jav.JAV_10_tabs.frame.setVisible(true);
 
     }  //  end JAV_xxxxx_visualize_10()
    
