e712609838338368cd83644bbd76981d3d924410
Yuri UTKIN <yu-2023@inbox.lv>
2023-02-01 11:15:03 +0200

    Update JAV_xxxxx_10.java

--- src/main/java/lv/yu/jav/xxxxx/JAV_xxxxx_10.java
+++ src/main/java/lv/yu/jav/xxxxx/JAV_xxxxx_10.java
@@ -32,9 +32,9 @@ public class JAV_xxxxx_10 {
         
     new lv.yu.jav.help.JAV_help_menu_help_10();
     new lv.yu.jav.lang.JAV_lang_menu_lang_10();
-	new lv.yu.jav.exit.JAV_exit_menu_exit_10();
+    new lv.yu.jav.exit.JAV_exit_menu_exit_10();
 	
-	lv.yu.jav.JAV_10_tabs.frame.setJMenuBar(lv.yu.jav.JAV_10_tabs.menubar);
+    lv.yu.jav.JAV_10_tabs.frame.setJMenuBar(lv.yu.jav.JAV_10_tabs.menubar);
 		
       }  //  end JAV_xxxxx_10()
         
