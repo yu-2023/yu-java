0affaa9ec9ec8fcfb0ce8b9f9eac49a9d6ec57bc
Yuri UTKIN <yu-2023@inbox.lv>
2023-03-08 14:00:41 +0200

    Add files via upload

--- run_cmd_jav/RUN --- JAVA_01-10.jar --- JAV_09_internal_frames_change.bat
+++ run_cmd_jav/RUN --- JAVA_01-10.jar --- JAV_09_internal_frames_change.bat
@@ -1,2 +1,2 @@
 
-%SystemRoot%\system32\cmd.exe /K "..\jdk15\bin\java.exe -cp ..\arh\JAVA_01-10.jar lv.yu.jav.JAV_09_internal_frames_change"
+%SystemRoot%\system32\cmd.exe /K "..\jdk15\jdk-15\bin\java.exe -cp ..\jar\JAVA_01-10.jar lv.yu.jav.JAV_09_internal_frames_change"
